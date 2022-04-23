d5fa6e72a255cd4237d077784c27d59aa0e385fb
Tonix22 <emiliotonix@gmail.com>
2022-04-23 15:57:40 -0500

    HWK5 ex1

---
+++ Notebooks/HW5/sist_com_BPSK.m
@@ -0,0 +1,44 @@
+% Programa sist_com_BPSK,
+% Esta rutina genera la simulation de un sistema de comunicaciones BPSK 
+%utilizando filtro formador, un filtro acoplado y un SNR predefinida 
+%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& 
+% &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&  
+% ------ TRANSMISOR -------------------- 
+% ----Se genera una serial BPSK con los siguientes datos: 
+%senalBPSK=BPSK_pb(num_bits,srate,filt,muest_porbit);
+
+clear all; close all;
+
+num_bits=10000; %Numero de bits a transmitir 
+srate=.01;      %Intervalo de muestreo
+filt='sqrt';   %Tipo de filtro transmisor, tambien puede ser cosa y rect 
+muest_porbit=5; %Numero de muestras por bit
+disp(num_bits)
+[filtrotrans,senalBPSK,t,senaldig]=BPSK_pb(num_bits,srate,filt,muest_porbit);
+
+%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&  
+% ------------ CANAL CON RUIDO ADITIVO -------------------------- 
+%— Se toma la señal y se le suma un ruido con potencia tal que se tiene una relacion 
+% señal a ruido especificada 
+%senal_masruido=awgn(SNR_deseado_dB,senalBPSK); 
+SNR_deseado_dB = 4; 
+senal_masruido = awgn(SNR_deseado_dB,senalBPSK);
+
+%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&  
+% ------ RECEPTOR CON FILTRO ACOPLADO --------------
+%La señal es pasada a travez de un filtro acoplado al filtro de transmisor 
+%para maximizar Ia relaciOn sepal a ruido 
+senal_recib=matched_filter(filtrotrans,senal_masruido);
+
+%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& 
+%------------- DISPOSITIVO DE DECISION ------------- 
+% La señal recibida es pasada por el circuido de decision 
+% que regresa el vector de bits detectados, asi como el niimero de bits erroneos contados 
+umbral=0; 
+[num_bits_erroneos,bits_detectadosj]=decision(umbral,senal_recib,num_bits,muest_porbit,t,senaldig,srate);
+
+%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& 
+% ------ GRAFICACION BER TEORICO Y SIMULADO --------------
+% Los bits con error se grafican sobre la curva teórica. La señal recibida es pasada por el circuito de decisión
+%que regresa el vector de bits detectados, así como el número de bits erróneos contados
+grafica_BERySNR(SNR_deseado_dB, num_bits_erroneos, num_bits);
--- funciones/BPSK_pb.m
+++ funciones/BPSK_pb.m
@@ -1,38 +1,50 @@
 function    [filtrotrans,senalBPSK,t,senaldig] =    BPSK_pb(num_bits,srate,filt,muest_porbit);
+%% Descripcion
+
+% function senal_BPSK=BSPK_pb(num_bits,srate,filt,muestr_porbit);
 
 %   Este progama genera una señal modulada BPSK en su representacion compleja pasabajas
-%   num_bits    especifica el numero de bits a transmitir, debe ser numero par y maor a 100;
-%   srate       especifica el intervalo de muestreo
-%   filt        especifica si se utiliza un filtro 'cosa' para coseno alzado, 'sqrt' para raiz de coseno alzado  y 'rect'    para un filtro rectandula
-%   muest_porbit    especificael numero de muestras por bit que se utilizaran en la simulacion, debe ser numero impar
+%   num_bits    : especifica el numero de bits a transmitir, debe ser numero par y mayor a 100;
+%   srate       : especifica el intervalo de muestreo
+%   filt        : especifica si se utiliza un filtro 
+%                'cosa' para coseno alzado, 
+%                'sqrt' para raiz de coseno alzado
+%                'rect'    para un filtro rectangular
+%  muest_porbit : especifica el numero de muestras por bit que se 
+%                 utilizaran en la simulacion, debe ser numero impar
 
-senaldig = round(rand(1,num_bits));
-senal       = 2*(senaldig-.5)        %se genera señal binaria de valores [1,-1]
+%% Implementacion
+senaldig = round(rand(1,num_bits)); %se puede utilizar se senaldig=randint(1,n)
+senal    = 2*(senaldig-.5);         %se genera señal binaria de valores [1,-1]
 
-% se produce la señal con el filtro formador
+% Se produce la señal con el filtro formador
 
-T   =   (num_bits + 10) * muest_porbit  *   srate;
-t   =   -T/2    :   srate   :   T/2;
-senaldiscretal  =   zeros(1,length(t)); %se genera la señal con deltas centradas en Ts
+T = (num_bits + 10)*muest_porbit*srate;
+t = -T/2 : srate : T/2;
+
+senaldiscretal = zeros(1,length(t)); %se genera la señal con deltas centradas en Ts
 cero   = find(t==0);
-senaldiscreta(cero-((num_bits/2)*muest_porbit)   :   muest_porbit    :   cero+((num_bits/2)*muest_porbit)-muest_porbit   ) =   senal;          %se generala señal con deltas centrada en Ts
-%se definen los valores de los pulsos
-pulsocuadrado   =   zeros(1,length(t));
-pulsocuadrado(  cero-((muest_porbit-1)/2)   :   cero+(  (muest_porbit-1/2))      ) = 1;
+half_len = ((num_bits/2)*muest_porbit);
+%se generala señal con deltas centrada en Ts
+senaldiscretal(cero-half_len: muest_porbit : cero+half_len-muest_porbit) = senal;
+
+%% Se definen los valores de los pulsos
+pulsocuadrado   = zeros(1,length(t));
+pulsocuadrado(int64(cero-((muest_porbit-1)/2)) : int64(cero+((muest_porbit-1/2)))) = 1;
 frcosine    =   zeros(1,length(t));
-if filt=='sqrt'
-    frcosine(   cero-(3*muest_porbit)   :   cero+3*muest_porbit)  =   rcosine(1,muest_porbit,'sqrt');
-
+if filt=="sqrt"
+    frcosine(cero-(3*muest_porbit):cero+3*muest_porbit) = rcosine(1,muest_porbit,'sqrt');
 else
-    frcosine(   cero-(3*muest_porbit)   :   cero+3*muest_porbit)  =   rcosine(1,muest_porbit);
+    frcosine(cero-(3*muest_porbit):cero+3*muest_porbit) = rcosine(1,muest_porbit);
 end
-
-if  filt=='rect'
+%% Elección del filtro
+if  filt=="rect"
     filtrotrans =   pulsocuadrado;
 else
     filtrotrans =   frcosine;
 end
 
+% estandirazion del filtro
 filtrotrans =   filtrotrans/sqrt(filtrotrans*filtrotrans');
 
 %---Se convolucionan los datos con el filtro transmisor
@@ -41,13 +53,13 @@ senalBPSK   =   convrec(senaldiscretal,filtrotrans);
 %---Se grafican los resultados
 figure;
 subplot(2,1,1), plot(t,senalBPSK);
-axis([-7.*srate.*muest_porbit  7.*srate.*muest_porbit-1.1    1.1])
+axis([-7*srate*muest_porbit 7*srate*muest_porbit -1.1 1.1]);
 xlabel('Señal BPSK generada en tiempo');
 hold on;
 subplot(2,1,2), stem(t,senaldiscretal, 'r');
 hold off;
 subplot(2,1,1), plot(t,filtrotrans);
-axis([-7*srate*muest_porbit 7*srate*muest_porbit-1.1 1.1]);
+axis([-7*srate*muest_porbit 7*srate*muest_porbit -1.1 1.1]);
 xlabel('Filtro de Transmisor Seleccionado');
 
 %------------------------- FIN DE FUNCION BPSK_pb.m ----------------------------
